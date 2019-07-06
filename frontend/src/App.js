d46be370ea4cdf2825ef60dcd26e022fad294137
Patrick Carlson <patcarls@cisco.com>
2019-07-06 19:53:14 -0400

    Adding navbar, and material ui as new css framework moving forward

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {HashRouter as Router, Route} from "react-router-dom";
 import './App.css';
+import MainNavbar from './MainNavbar';
+
+
+class Main extends Component{
+  render() {
+    return (
+      <div className="main">
+        <h1> OwO </h1>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="content">
+          <MainNavbar />
+          <div classNme="container">
+            <Route exact path="/" component={Main} />
+          </div>
+        </div>
+      </Router>
     </div>
   );
 }
