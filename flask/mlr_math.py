5f20a52480b7590c1cbddf62036db13d97769428
Patrick Carlson <patcarls@cisco.com>
2019-07-15 21:50:15 -0400

    Pitch Matrix

--- flask/mlr_math.py
+++ flask/mlr_math.py
@@ -13,37 +13,53 @@ def build_matrix(pitch_list):
         (801,900),
         (901,1000),
     ]
-    range_dict = {
-        "0":0,
-        "1":0,
-        "2":0,
-        "3":0,
-        "4":0,
-        "5":0,
-        "6":0,
-        "7":0,
-        "8":0,
-        "9":0,
-
-    }
+    
     pitch_dict = {
-        "HR":range_dict.copy(),
-        "3B":range_dict.copy(),
-        "2B":range_dict.copy(),
-        "1B":range_dict.copy(),
-        "BB":range_dict.copy(),
-        "FO":range_dict.copy(),
-        "K":range_dict.copy(),
-        "PO":range_dict.copy(),
-        "RGO":range_dict.copy(),
-        "LGO":range_dict.copy(), 
+        "HR":0,
+        "3B":0,
+        "2B":0,
+        "1B":0,
+        "BB":0,
+        "FO":0,
+        "K":0,
+        "PO":0,
+        "RGO":0,
+        "LGO":0,
+        "IBB":0,
     }
-    for pitch_set in pitch_list:
-        pitch_val = pitch_set['pitch']
-        pitch_result = pitch_set['result']
-        for x in range(0,len(ranges)):
-            if pitch_val >= ranges[x][0] and pitch_val <= ranges[x][1]:
+    range_dict = {
+        "0":{**{"range":"1-100"}, **pitch_dict.copy()},
+        "1":{**{"range":"101-200"}, **pitch_dict.copy()},
+        "2":{**{"range":"201-300"}, **pitch_dict.copy()},
+        "3":{**{"range":"301-400"}, **pitch_dict.copy()},
+        "4":{**{"range":"401-500"}, **pitch_dict.copy()},
+        "5":{**{"range":"501-600"}, **pitch_dict.copy()},
+        "6":{**{"range":"601-700"}, **pitch_dict.copy()},
+        "7":{**{"range":"701-900"}, **pitch_dict.copy()},
+        "8":{**{"range":"801-900"}, **pitch_dict.copy()},
+        "9":{**{"range":"901-1000"}, **pitch_dict.copy()},
+    }
+    # pitches = [(x['pitch'],x['result']) for x in pitch_list]
+    # # next_pitch = 0
+    # for x in range(0,len(pitches)):
+    print(range_dict)
 
-                pitch_dict[pitch_result][str(x)] += 1
-                break
-    return pitch_dict
+    for x in range(0,len(pitch_list)-1):
+        pitch_set = pitch_list[x]
+        # print(pitch_set)
+        # pitch_val = pitch_set['pitch']
+        # if x + 1 != len(pitch_list):
+        next_val = pitch_list[x + 1]['pitch']
+        if next_val:
+            # continue
+
+            pitch_result = pitch_set['result']
+            for x in range(0,len(ranges)):
+                if next_val >= ranges[x][0] and next_val <= ranges[x][1]:
+                    range_dict[str(x)][pitch_result] += 1
+                    break
+    ret_list = []
+    for key, value in range_dict.items():
+        ret_list.append(value)
+    return ret_list
+    
