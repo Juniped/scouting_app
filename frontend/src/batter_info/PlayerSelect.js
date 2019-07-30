ccc53b5906c182b109713ffe78823927b6eeb245
Patrick Carlson <pats.carlson@gmail.com>
2019-07-30 01:37:26 -0400

    made Tabs nicer, and cleaned up warnings across the whole thing. Also made the graph work on Mobile

--- frontend/src/batter_info/PlayerSelect.js
+++ frontend/src/batter_info/PlayerSelect.js
@@ -45,7 +45,7 @@ class PlayerSelect extends Component {
         let d = [];
         for (let i = 0; i < this.props.players.length; i++) {
             let player = this.props.players[i]
-            d.push(<option value={player.id} > {player.name} </option>)
+            d.push(<option value={player.id} key={i}> {player.name} </option>)
         }
         return d;
     }
@@ -64,7 +64,7 @@ class PlayerSelect extends Component {
                         Select a Player
                     </Typography>
                     <FormControl className={classes.formControl}>
-                        <NativeSelect native value={this.state.currentPlayer} onChange={this.handleChange} inputProps={{ name: 'currentPlayer', id: 'player-native-simple', }}>
+                        <NativeSelect value={this.state.currentPlayer} onChange={this.handleChange} inputProps={{ name: 'currentPlayer', id: 'player-native-simple', }}>
                             <option value="" />
                             {this.getOptions()}
                         </NativeSelect>
